15f50b1af9ec55b70f23d33a140cd770282c0973
CovERUshKA <ricovalki@gmail.com>
2023-01-22 17:18:02 +0300

    Revert "Добавил округление до сотых avg_hum и avg_temp"
    This reverts commit acc0b2c386d37d61c512246d40c90d0af4f24629.

--- predprof_teplici/collector.py
+++ predprof_teplici/collector.py
@@ -5,6 +5,7 @@ import asyncio
 import requests
 import database as db
 
+end_working = False
 
 def get_air_temp_hum(id):
     response = requests.get(f"{config.url_get_temp_hum}/{id}")
@@ -20,40 +21,32 @@ def get_air_temp_hum(id):
 async def aget_ground_hum(session : aiohttp.ClientSession, id):
     async with session.get(f"{config.url_get_hum}/{id}") as resp:
         jsoned = {}
-
         if resp.status == 200:
             jsoned = await resp.json(content_type=None)
-
             if jsoned == None:
                 jsoned = {}
-
         humidity = jsoned.get("humidity", None)
-
         return humidity
-
 
 # Влажность и температура воздуха
 async def aget_air_temp_hum(session : aiohttp.ClientSession, id):
     async with session.get(f"{config.url_get_temp_hum}/{id}") as resp:
         jsoned = {}
-        
         if resp.status == 200:
             jsoned = await resp.json(content_type=None)
-
             if jsoned == None:
                 jsoned = {}
-
         humidity = jsoned.get("humidity", None)
         temperature = jsoned.get("temperature", None)
-
         return temperature, humidity
 
-
 async def infinite_collect():
-    async with aiohttp.ClientSession() as session:
-        while True:
-            tasks = []
+    print("Collector started")
 
+    async with aiohttp.ClientSession() as session:
+        while end_working == False:
+
+            tasks = []
             for sensor_id in range(1, 7):
                 tasks.append(asyncio.ensure_future(aget_ground_hum(session, sensor_id)))
 
@@ -80,12 +73,12 @@ async def infinite_collect():
             avg_hum = None
 
             if len(temps) != 0:
-                avg_temp = round(sum(temps) / len(temps), 2)
-
+                avg_temp = sum(temps) / len(temps)
             if len(hums) != 0:
-                avg_hum = round(sum(hums) / len(hums), 2)
-            
+                avg_hum = sum(hums) / len(hums)
+
             db.add_data_from_sensors(all_ground_humidity, all_air_temp_hum, avg_temp, avg_hum, time_collected)
 
             time.sleep(5)
-            
+    
+    print("Collector ended")
