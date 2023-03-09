412d755e14ce2fed4de39ebe008408ce18200eba
Mertysh <100412812+Mertysh@users.noreply.github.com>
2023-03-09 17:47:58 +0300

    fix

--- predprof_teplici/main.py
+++ predprof_teplici/main.py
@@ -5,9 +5,9 @@ from __init__ import create_app
 import collector
 
 
-# def run_app(first, second):
+def run_app(first, second):
     # print(f'{first=}\n\n{second=}')
-if __name__ == '__main__':
+# if __name__ == '__main__':
     app = create_app()
 
     t = threading.Thread(target=asyncio.run, args=(collector.infinite_collect(),))
