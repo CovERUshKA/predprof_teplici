5219258035f915adf4aa5ecc8a2f78130aa9727a
Mertysh <100412812+Mertysh@users.noreply.github.com>
2023-03-09 23:46:25 +0300

    fix

--- predprof_teplici/database.py
+++ predprof_teplici/database.py
@@ -5,7 +5,7 @@ from rest import get_current_time
 lock = threading.Lock()
 
 # Подключаемся к файлу базы данных
-con = sqlite3.connect("./data/main.db", check_same_thread=False)
+con = sqlite3.connect("app/predprof_teplici/data/main.db", check_same_thread=False)
 # Берём управление базой данных
 cur = con.cursor()
 
