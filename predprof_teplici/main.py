7d91329e8f1824a4f51e16862e69802adb5c5243
Mertysh <100412812+Mertysh@users.noreply.github.com>
2023-03-09 17:40:31 +0300

    fix

--- predprof_teplici/main.py
+++ predprof_teplici/main.py
@@ -5,14 +5,15 @@ from __init__ import create_app
 import collector
 
 
-def run_app(first, second):
-    print(f'{first=}\n\n{second=}')
+# def run_app(first, second):
+    # print(f'{first=}\n\n{second=}')
+if __name__ == '__main__':
     app = create_app()
 
     t = threading.Thread(target=asyncio.run, args=(collector.infinite_collect(),))
     t.start()
 
-    app.run(debug=False,  port=80, host="0.0.0.0")
+    # app.run(debug=False,  port=80, host="0.0.0.0")
 
     print("flask ended")
 
