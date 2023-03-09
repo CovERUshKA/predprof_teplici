3ab63ae83e66e5a47c60c9615f50d289f775289b
Mertysh <100412812+Mertysh@users.noreply.github.com>
2023-03-09 23:03:36 +0300

    fix

--- predprof_teplici/main.py
+++ predprof_teplici/main.py
@@ -2,18 +2,15 @@ import asyncio
 import threading
 
 from __init__ import create_app 
-import collect.collector
 
 
 def app(first, second):
     print(f'{first=}\n\n{second=}')
     app = create_app()
 
-    t = threading.Thread(target=asyncio.run, args=(collector.infinite_collect(),))
+    t = threading.Thread(target=asyncio.run)
     t.start()
 
     app.run(port=5000)
 
-    print("flask ended")
-
-    collector.end_working = True
+    print("flask ended")    
