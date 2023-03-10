b29b1eff52982167890ad93d5c4f909e1ff208a8
Mertysh <100412812+Mertysh@users.noreply.github.com>
2023-03-10 21:51:23 +0300

    fix

--- predprof_teplici/main.py
+++ predprof_teplici/main.py
@@ -1,13 +1,6 @@
 import asyncio
 import threading
 
-from __init__ import create_app 
 
 
-if __name__ == '__main__':
-    app = create_app()
 
-    # t = threading.Thread(target=asyncio.run)
-    # t.start()
-
-    app.run()
