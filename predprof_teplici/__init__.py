3a5448c8925d4f803f0a7326e883bb542b49a718
Mertysh <100412812+Mertysh@users.noreply.github.com>
2023-03-10 22:13:03 +0300

    fix

--- predprof_teplici/__init__.py
+++ predprof_teplici/__init__.py
@@ -3,36 +3,59 @@ import json
 from flask import Flask, render_template
 from flask_cors import CORS
 
-def create_app():
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
+# def create_app():
+#     # create and configure the app
+#     CORS(app)
 
-    app.config["settings"] = {
-        "parameters": {
-            "T": None,
-            "H": None,
-            "Hb": None
-        },
-        "fork_drive": 0,
-        "total_hum": 0,
-        "emergency": 0,
-        "watering": [0, 0, 0, 0, 0, 0]
-    }
+#     app.config["settings"] = {
+#         "parameters": {
+#             "T": None,
+#             "H": None,
+#             "Hb": None
+#         },
+#         "fork_drive": 0,
+#         "total_hum": 0,
+#         "emergency": 0,
+#         "watering": [0, 0, 0, 0, 0, 0]
+#     }
+
+#     # http://127.0.0.1:80
+#     @app.route('/')
+#     def index():
+#         return render_template("index.html")
+
+#     import api
+#     app.register_blueprint(api.bp)
+
+#     return app
+
 
     # http://127.0.0.1:80
-    @app.route('/')
-    def index():
-        return render_template("index.html")
 
-    import api
-    app.register_blueprint(api.bp)
 
-    return app
+app = Flask(__name__, instance_relative_config=True)
+CORS(app)
+
+app.config["settings"] = {
+    "parameters": {
+        "T": None,
+        "H": None,
+        "Hb": None
+    },
+    "fork_drive": 0,
+    "total_hum": 0,
+    "emergency": 0,
+    "watering": [0, 0, 0, 0, 0, 0]
+}
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app = create_app()
+    # app = create_app()
 
     # t = threading.Thread(target=asyncio.run)
     # t.start()
